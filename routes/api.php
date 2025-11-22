6689af26a229e5ed92a8194649b4bf8eef1dcc44
“[Jagdish <[jsyadav.1996@gmail.com]>
2025-11-22 23:39:00 +0530

    Updated routes into api.php

--- routes/api.php
+++ routes/api.php
@@ -27,13 +27,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
 
     // Product API Routes
-    Route::get('/products', [ProductController::class, 'index']);
-    Route::post('/products', [ProductController::class, 'store']);
-    Route::get('/products/{id}', [ProductController::class, 'show']);
-    Route::put('/products/{id}', [ProductController::class, 'update']);
-    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::apiResource('products', ProductController::class);
 
     // Appointment API Routes
-    Route::get('/appointments', [AppointmentController::class, 'index']);
-    Route::post('/appointments', [AppointmentController::class, 'store']);
+    Route::apiResource('appointments', AppointmentController::class)->only(['index', 'store']);
 });
